19adcd7b29ffbf5bc83965f1c45c049e20da704f
Anny Cabrera <acabrera@harding.edu>
2023-07-12 23:43:23 -0600

    Eventos

--- Practice/script.js
+++ Practice/script.js
@@ -1,26 +1,11 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafoClass = document.getElementsByClassName('parrafoClass');
-const parrafoId = document.getElementById('parrafoId');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
 
-console.log(h1);
-console.log({
-  h1,
-  p,
-  parrafoClass,
-  parrafoId,
-  input,
-});
-
-h1.innerText = "NCT 127";
-
-h1.classList.add("rojo");
-h1.classList.add("verde");
-h1.classList.remove("rojo");
-
-const img = document.createElement('img');
-img.setAttribute('src', 'https://images.pexels.com/photos/13282615/pexels-photo-13282615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
-
-parrafoId.append(img);
+function btnOnClick(){
+ const suma = Number(input1.value) + Number(input2.value);
+ pResult.innerText = "Resultado : "+ suma;
+}
 
