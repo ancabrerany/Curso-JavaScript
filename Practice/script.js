0e640883acdea2172f307b1731e7f170d8805034
Anny Cabrera <acabrera@harding.edu>
2023-07-17 15:40:14 -0600

    manipulacion del dom

--- Practice/script.js
+++ Practice/script.js
@@ -1,10 +1,13 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
-function btnOnClick(){
+form.addEventListener('click', sumarInput);
+
+function sumarInput(){
  const suma = Number(input1.value) + Number(input2.value);
  pResult.innerText = "Resultado : "+ suma;
 }
